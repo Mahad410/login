be08a7a69ed2038d4f42ed5862647cce7754371b
Mahad410 <mahadqureshi410@gmail.com>
2022-06-05 15:26:49 -0600

    Base Apparel coming soon page for frontend mentor using html, css and javascript

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById('sub_btn');
+const btn = document.getElementById('subBtn');
 const errMsg = document.getElementById('err');
 const email = document.getElementById('email');
 email.addEventListener('focus', () => {
@@ -20,9 +20,9 @@ btn.addEventListener('click', () => {
 
     if (email.value <= 0 || email.value == null || email.value == undefined || (email.value.indexOf('.com') == -1)) {
         errMsg.style.visibility = 'visible';
+        btn.style.setProperty('--visibility', 'visible');
         return false;
     } else {
-        return true;
         btn.style.setProperty('--visibility', 'hidden');
     }
 
