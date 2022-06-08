7b66d829bb45b31baf5f1266375a5fa125895d43
Mahad410 <mahadqureshi410@gmail.com>
2022-06-07 23:33:05 -0600

    Intro component with sign-up form with form validation

--- main.js
+++ main.js
@@ -1,29 +1,75 @@
-const btn = document.getElementById('subBtn');
-const errMsg = document.getElementById('err');
+const btn = document.getElementById('btn');
+const errMsg = document.querySelectorAll('.err_msg');
+const fname = document.getElementById('f_name');
+const lname = document.getElementById('l_name');
 const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+
+btn.addEventListener('click', () => {
+    if (fname.value <= 0 || fname.value == null || fname.value == undefined) {
+        errMsg[0].style.setProperty('--visible', 'visible');
+    }
+    if (lname.value <= 0 || lname.value == null || lname.value == undefined) {
+        errMsg[1].style.setProperty('--visible', 'visible');
+    }
+    if (email.value <= 0 || email.value == null || email.value == undefined || email.value == '' || (email.value.indexOf('.com') == -1)) {
+        errMsg[2].style.setProperty('--visible', 'visible');
+    }
+    if (pass.value <= 0 || pass.value == null || pass.value == undefined || pass.value == '') {
+        errMsg[3].style.setProperty('--visible', 'visible');
+    } else {
+        return true;
+    }
+})
+
+fname.addEventListener('focus', () => {
+    errMsg[0].style.setProperty('--visible', 'hidden');
+})
+
+fname.addEventListener('focusout', () => {
+    if (fname.value <= 0 || fname.value == null || fname.value == undefined) {
+        errMsg[0].style.setProperty('--visible', 'visible');
+        return false;
+    } else {
+        errMsg[0].style.setProperty('--visible', 'hidden');
+    }
+})
+
+lname.addEventListener('focus', () => {
+    errMsg[1].style.setProperty('--visible', 'hidden');
+})
+
+lname.addEventListener('focusout', () => {
+    if (fname.value <= 0 || fname.value == null || fname.value == undefined) {
+        errMsg[1].style.setProperty('--visible', 'visible');
+        return false;
+    } else {
+        errMsg[1].style.setProperty('--visible', 'hidden');
+    }
+})
+
 email.addEventListener('focus', () => {
-    errMsg.style.visibility = 'hidden';
-    btn.style.setProperty('--visibility', 'hidden');
+    errMsg[2].style.setProperty('--visible', 'hidden');
 })
 
 email.addEventListener('focusout', () => {
     if (email.value <= 0 || email.value == null || email.value == undefined || (email.value.indexOf('.com') == -1)) {
-        errMsg.style.visibility = 'visible';
-        btn.style.setProperty('--visibility', 'visible');
+        errMsg[2].style.setProperty('--visible', 'visible');
         return false;
     } else {
-        errMsg.style.visibility = 'hidden';
+        errMsg[2].style.setProperty('--visible', 'hidden');
     }
-    console.log(email.value);
 })
-btn.addEventListener('click', () => {
 
-    if (email.value <= 0 || email.value == null || email.value == undefined || (email.value.indexOf('.com') == -1)) {
-        errMsg.style.visibility = 'visible';
-        btn.style.setProperty('--visibility', 'visible');
+pass.addEventListener('focus', () => {
+    errMsg[3].style.setProperty('--visible', 'hidden');
+})
+
+pass.addEventListener('focusout', () => {
+    if (pass.value <= 0 || pass.value == null || pass.value == undefined || pass.value == '') {
+        errMsg[3].style.setProperty('--visible', 'visible');
         return false;
     } else {
-        btn.style.setProperty('--visibility', 'hidden');
+        errMsg[3].style.setProperty('--visible', 'hidden');
     }
-
 })
